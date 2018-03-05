fa8ecd2d2a87985330419d0c3c524d2abc7d7cd8
Repal, Ninad <ninad.repal.vz@renesas.com>
2018-03-05 11:29:25 -0600

    Created Pawn objects

--- pieces/Piece.py
+++ pieces/Piece.py
@@ -8,6 +8,8 @@ Created on Feb 26, 2018
 
 MOVE_FLAG = False
 KILL_FLAG = False
+WHITE = 'white'
+BLACK = 'black'
 black_position_list = []
 white_position_list = []
 
@@ -16,9 +18,9 @@ def check_position(x, y):
 
 def check_square_color(x, y):
     if x + y & 1:
-        return 'black' # odd: 6 & 1 = 0
+        return BLACK # odd: 6 & 1 = 0
     else:
-        return 'white'
+        return WHITE
     
 class Piece(object):
     '''
@@ -111,8 +113,11 @@ class Black(Pawn, Rook, Bishop, Knight, Queen, King):
         super().__init__(x, y, 'black')
         
         
-n = Black()
 
 
-p = Pawn(3,4, 'black')
-print(p.x)
+# when you kill the piece remove the piece from the position list and the 
+# piece object dictionaries that are created herewith
+
+black_pawns = {x: Pawn(x, 7, WHITE) for x in range(1, 9)}
+white_pawns = {x: Pawn(x, 2, BLACK) for x in range(1, 9)}
+
