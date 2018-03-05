a3fc8bf0c62c299fe203fca66e6a765a2846941c
Repal, Ninad <ninad.repal.vz@renesas.com>
2018-03-05 17:02:11 -0600

    added creation of pawn rook and knight objects

--- pieces/Piece.py
+++ pieces/Piece.py
@@ -1,26 +1,68 @@
+
 '''
 Created on Feb 26, 2018
 
 @author: NREPAL
 '''
 
+"""
+move() : in the definition of this function, consider the following checks:
+    i)check if there is any piece in the position specified
+    ii) check if the piece(if opposite color) can be killed with the active piece 
+    by moving to the desired position
+    iii)check if the active piece can move to the desired position
+"""
 
-
-MOVE_FLAG = False
-KILL_FLAG = False
 WHITE = 'white'
 BLACK = 'black'
+MOVE_FLAG = False
+KILL_FLAG = False
 black_position_list = []
 white_position_list = []
+black_killed_list = []
+white_killed_list = []
+piece_positions = {}
 
-def check_position(x, y):
-    pass
+def check_piece_at_position(x, y):
+    position = (x, y)
+    piece = list(piece_positions.keys())[list(
+            piece_positions.values()).index(position)]
+    return piece
 
 def check_square_color(x, y):
     if x + y & 1:
-        return BLACK # odd: 6 & 1 = 0
+        return 'black' # odd: 6 & 1 = 0
     else:
-        return WHITE
+        return 'white'
+
+def kill_piece(x, y):
+    """
+    check if (x,y) in white_position_list
+    check if (x,y) in black_position_list
+    if yes: put in killed_pieces
+    """
+    KILL_FLAG = False
+    try:
+        piece_positions.pop(check_piece_at_position(x, y))
+        KILL_FLAG = True
+    except KeyError:
+        print("No piece found to be killed")
+        KILL_FLAG = False
+    
+    return KILL_FLAG
+   
+
+def update_piece_position(piece, x, y):
+    piece_positions[piece] = (x, y)
+    
+def check_for_check(self, x, y):
+    """
+    check to see of the opposite color team gets a check after this move
+    for eg: if the black color is playing a move, check to see if after the
+            move, the black king gets a check
+            return true if check else return false
+    """
+    pass
     
 class Piece(object):
     '''
@@ -75,6 +117,7 @@ class Rook(Piece):
         if self.color == 'black':
             if (x, y) in white_position_list:
                 pass
+                
             
                 
 class Knight(Piece):
@@ -101,23 +144,83 @@ class Queen(Piece):
     
     def __init__(self, x, y, color):
         super().__init__(x, y, color)
+        
+    def move(self, x, y):
+        if self.color == 'black':
+            if (x, y) in white_position_list:
+                if not check_for_check(self, x, y):
+                    if kill_piece(x, y):
+                        self.x = x
+                        self.y = y
+                        MOVE_FLAG = True
+                    else:
+                        MOVE_FLAG = False
+
 
 class King(Piece):
-    
+
     def __init__(self, x, y, color):
         super().__init__(x, y, color)
 
 
-class Black(Pawn, Rook, Bishop, Knight, Queen, King):
-    def __init__(self, x, y, color):
-        super().__init__(x, y, 'black')
+def create_pieces():
+    black_pawn = {x: Pawn(x, 7, BLACK) for x in range(1, 9)}
+    white_pawn = {x: Pawn(x, 2, WHITE) for x in range(1, 9)}
+
+    black_rook = {num + 1: Rook(x, 8, BLACK) for num, x in enumerate([1,8])}
+    white_rook = {num + 1: Rook(x, 1, WHITE) for num, x in enumerate([1,8])}
+    
+    black_knight = {num + 1: Knight(x, 8, BLACK) for num, x in enumerate([2,7])}
+    white_knight = {num + 1: Knight(x, 1, WHITE) for num, x in enumerate([2,7])}
+    
+    print(black_rook[2].position)
+
+    
+    bb1 = Bishop(3, 8, 'black')
+    bb2 = Bishop(6, 8, 'black')
+    bw1 = Bishop(3, 1, 'white')
+    bw2 = Bishop(6, 1, 'white')
+    
+    knb1 = Knight(2, 8, 'black')
+    knb2 = Knight(7, 8, 'black')
+    knw1 = Knight(2, 1, 'white')
+    knw2 = Knight(7, 1, 'white')
+    
+    rb1 = Queen(4, 8, 'black')
+    rw1 = Queen(4, 1, 'white')
+    
+    kb1 = King(5, 8, 'black')
+    kw1 = King(5, 1, 'white')
+    
+
+
+create_pieces()
         
-        
+class Board(object):
+    def __init__(self,x, y):
+        self.x = x
+        self.y = y
+    
+    def print_board(self):
+        pass
+    
+def print_board():
+    print(u' \u265C    \u265E     \u265D    \u265B     \u265A    \u265D    \u265E     \u265B')
+    print(u' \u265F    \u265F     \u265F    \u265F     \u265F    \u265F    \u265F     \u265F')
+#     print(u'\u25FB\u25FC\u25FB\u25FC\u25FB\u25FC\u25FB\u25FC ')
+    print(" -  -  -  -  -  -  -  -")
+    print(" -  -  -  -  -  -  -  -")
+    print(" -  -  -  -  -  -  -  -")
+    print(" -  -  -  -  -  -  -  -")
+    print(" -  -  -  -  -  -  -  -")
+    print(u'\u2659    \u2659     \u2659    \u2659     \u2659    \u2659    \u2659     \u2659')
+    print(u'\u2656    \u2658     \u2657    \u2655     \u2654    \u2657    \u2658     \u2656')
+    
+
 
 
 # when you kill the piece remove the piece from the position list and the 
 # piece object dictionaries that are created herewith
 
-black_pawns = {x: Pawn(x, 7, WHITE) for x in range(1, 9)}
-white_pawns = {x: Pawn(x, 2, BLACK) for x in range(1, 9)}
+
 
